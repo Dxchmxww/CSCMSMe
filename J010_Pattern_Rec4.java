9f82f7943ce38d8c7fe29a32917984e4093fbbb4
Dxchmxww <120299511+Dxchmxww@users.noreply.github.com>
2022-12-11 15:36:50 +0700

    Add files via upload

---
+++ J010_Pattern_Rec4.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class J010_Pattern_Rec4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+        int col = sc.nextInt();
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if( i==0 || j==0 || i==row-1 || j==col-1){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print("-");
+                }
+            }
+            System.out.println();
+            
+        }
+    }
+}
+

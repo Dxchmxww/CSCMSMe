6e4566e4c7f7e1b5cc6aff258bc0a8827eb7eb21
Dxchmxww <120299511+Dxchmxww@users.noreply.github.com>
2022-12-11 15:38:36 +0700

    Add files via upload

---
+++ J040_ThesumofN.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class J040_ThesumofN {
+    public static void main(String[] args){
+      Scanner sc = new Scanner(System.in);
+      int x = sc.nextInt();
+      long sum = 0;
+      
+      for(int i = 1; i<=x; i++){
+         sum += i;
+      }
+      
+      System.out.print(sum);
+    }
+}
